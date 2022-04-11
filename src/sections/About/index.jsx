dbc02fb1a9d9d07a70cf1c866470f8558533a665
Marcos <marcosmafeimachado@gmail.com>
2022-04-10 22:37:36 -0300

    fix: fixed the about me section button

--- src/sections/About/index.jsx
+++ src/sections/About/index.jsx
@@ -78,22 +78,9 @@ const About = ({ current, page = false }) => {
               <div className="mini--container">
                 <h4>Passionate about technology, games and animals</h4>
                 {page ? (
-                  <Button
-                    onClick={() => {
-                      setCurrent(1);
-                      history.push("/");
-                    }}
-                  >
-                    Show less
-                  </Button>
+                  <Button url="/">Show less</Button>
                 ) : (
-                  <Button
-                    onClick={() => {
-                      history.push("/about");
-                    }}
-                  >
-                    Show more
-                  </Button>
+                  <Button url="/about">Show more</Button>
                 )}
               </div>
             </Infos>
